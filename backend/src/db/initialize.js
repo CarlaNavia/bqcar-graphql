834a8eaf02723c0f7e7fa5e710717d52fc1537cf
Carla Navia <cnavia27@gmail.com>
2021-02-12 12:26:12 +0100

    updated mongoose connection

--- backend/src/db/initialize.js
+++ backend/src/db/initialize.js
@@ -1,6 +1,10 @@
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
 
 mongoose
-  .connect('mongodb://localhost/bq-car')
-  .then(() => console.log('🎯 Mongodb connected'))
-  .catch(error => console.log(error))
+  .connect("mongodb://localhost/bq-car", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log("🎯 Mongodb connected"))
+  .catch((error) => console.log(error));
